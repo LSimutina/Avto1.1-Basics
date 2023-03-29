4cb1e110641a12e8c6fe26e21d1b87da6762554f
Lidiya <simutina.1994@mail.ru>
2023-03-29 22:43:29 +0300

    Add TestNG-1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,53 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void shouldSpendForMaxCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 100;
+        int actual = service.remain(900);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldSpendForMaxCashbackIfAmountNull() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldSpendForMaxCashbackNull() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void upperBoundForMaxCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void lowerBoundForMaxCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        assertEquals(actual, expected);
+    }
 }
